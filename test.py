1bff0136c175ff150c17c52c6b29552c68a9b44b
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2019-05-09 19:41:07 +0200

    dummy test of abc

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-from dnn-utils.model import BaseModel
+from dnn_utils.model import BaseModel
+
+class MyModel(BaseModel) :
+
+    def print_name(self) :
+        print("MyModel::print_name")
+
+model = MyModel()
+model.print_name()
